e3a576d9c84284e2e0050b5b608c108cb659167a
Nguyen Lam Vy Dat <101176244+VyDat-1702@users.noreply.github.com>
2025-03-26 19:46:23 +0700

    Add files via upload

--- 22139012.py
+++ 22139012.py
@@ -31,18 +31,18 @@ miniBatch_size = 1000
 
 # Initialize parameters
 Wh1_init = np.random.uniform(low=-0.5, high=0.5, size=(h1_units, inputs_size)) 
-bh1_init = np.zeros((1, h1_units))                                          
+bj_init = np.zeros((1, h1_units))                                          
 Wh2_init = np.random.uniform(low=-0.5, high=0.5, size=(h2_units, h1_units))
-bh2_init = np.zeros((1, h2_units))                                            
+bl_init = np.zeros((1, h2_units))                                            
 Wo_init = np.random.uniform(low=-0.5, high=0.5, size=(num_classes, h2_units)) 
-bo_init = np.zeros((1, num_classes))
+bk_init = np.zeros((1, num_classes))
 
 print(f"Wh1_init: {Wh1_init.shape}")
-print(f"bh1_init: {bh1_init.shape}")
+print(f"bh1_init: {bj_init.shape}")
 print(f"Wh2_init: {Wh2_init.shape}")
-print(f"bh2_init: {bh2_init.shape}")
+print(f"bh2_init: {bl_init.shape}")
 print(f"Wo_init: {Wo_init.shape}")
-print(f"bo_init: {bo_init.shape}")
+print(f"bo_init: {bk_init.shape}")
 
 # Activation functions
 sigmoid = lambda x: 1. / (1. + np.exp(-x))
@@ -54,12 +54,12 @@ def softmax(x):
 ReLU = lambda x: np.maximum(0, x)
 
 # Forward pass
-def Forwardpass(X, Wh1, bh1, Wh2, bh2, Wo, bo):
-    zh1 = np.matmul(X, Wh1.T) + bh1 
+def Forwardpass(X, Wh1, bj, Wh2, bl, Wo, bk):
+    zh1 = np.matmul(X, Wh1.T) + bj
     a1 = ReLU(zh1)        
-    zh2 = np.matmul(a1, Wh2.T) + bh2 
+    zh2 = np.matmul(a1, Wh2.T) + bl 
     a2 = sigmoid(zh2)     
-    z =np.matmul(a2, Wo.T) + bo  
+    z =np.matmul(a2, Wo.T) + bk
     o = softmax(z)         
     return zh1, a1, zh2, a2, o
 
@@ -69,25 +69,25 @@ def AccTest(label, prediction):
 
 
 # Train function
-def train(x_train, y_train, x_test, y_test, train_samples, batch_size, Wh1, bh1, Wh2, bh2, Wo, bo, epochs, lr):
+def train(x_train, y_train, x_test, y_test, train_samples, batch_size, Wh1, bj, Wh2, bl, Wo, bk, epochs, lr):
     loss_history = []
     acc_history = []
     
-    print("\n-----Neural Network Mini-Batch GD Summary-----")
+    print("\n----- Neural Network Mini-Batch GD -----")
     print(f"Input Size: (1000, 784) {inputs_size*batch_size}")
     print("Hidden Layer 1:")
     print(f"\tActivation Shape: ({miniBatch_size}, {h1_units})")
     print(f"\tActivation Size: {miniBatch_size * h1_units}")
-    print(f"\tParameters: {Wh1_init.size + bh1_init.size}")
+    print(f"\tParameters: {Wh1_init.size + bj_init.size}")
     print("Hidden Layer 2:")
     print(f"\tActivation Shape: ({miniBatch_size}, {h2_units})")
     print(f"\tActivation Size: {miniBatch_size * h2_units}")
-    print(f"\tParameters: {Wh2_init.size + bh2_init.size}")
+    print(f"\tParameters: {Wh2_init.size + bl_init.size}")
     print("Output Layer:")
     print(f"\tActivation Shape: ({miniBatch_size}, {num_classes})")
     print(f"\tActivation Size: {miniBatch_size * num_classes}")
-    print(f"\tParameters: {Wo_init.size + bo_init.size}")
-    print(f"Total parameter: {Wh1_init.size + bh1_init.size + Wh2_init.size + bh2_init.size + Wo_init.size + bo_init.size}\n")
+    print(f"\tParameters: {Wo_init.size + bk_init.size}")
+    print(f"Total parameter: {Wh1_init.size + bj_init.size + Wh2_init.size + bl_init.size + Wo_init.size + bk_init.size}\n")
     
     for ep in range(1, epochs + 1):
         Stochastic_samples = np.arange(train_samples)
@@ -100,7 +100,7 @@ def train(x_train, y_train, x_test, y_test, train_samples, batch_size, Wh1, bh1,
             y = y_train[Batch_samples, :]
             
             # Forward pass
-            zh1, a1, zh2, a2, o = Forwardpass(x, Wh1, bh1, Wh2, bh2, Wo, bo)
+            zh1, a1, zh2, a2, o = Forwardpass(x, Wh1, bj, Wh2, bl, Wo, bk)
             
             # Compute loss
             batch_loss = -np.sum(np.multiply(y, np.log(o + 1e-9))) / batch_size
@@ -114,43 +114,42 @@ def train(x_train, y_train, x_test, y_test, train_samples, batch_size, Wh1, bh1,
             dh1s = np.multiply(dh1, (zh1 > 0))
             
             dWo = np.matmul(d.T, a2)
-            dbo = np.mean(d, axis=0)
+            dbo = np.sum(d, axis=0)
             dWh2 = np.matmul(dh2s.T, a1)
-            dbh2 = np.mean(dh2s, axis=0)
+            dbh2 = np.sum(dh2s, axis=0)
             dWh1 = np.matmul(dh1s.T, x)
-            dbh1 = np.mean(dh1s, axis=0)
+            dbh1 = np.sum(dh1s, axis=0)
             
             # Update parameters
             Wo -= lr * dWo / batch_size
-            bo -= lr * dbo
+            bk -= lr * dbo  / batch_size
             Wh2 -= lr * dWh2 / batch_size
-            bh2 -= lr * dbh2
+            bl -= lr * dbh2  / batch_size
             Wh1 -= lr * dWh1 / batch_size
-            bh1 -= lr * dbh1
+            bj -= lr * dbh1 / batch_size
             
         # Save loss
         loss_history.append(epoch_loss / (train_samples // batch_size))
         
         # Compute accuracy
-        _, _, _, _, prediction = Forwardpass(x_test, Wh1, bh1, Wh2, bh2, Wo, bo)
+        _, _, _, _, prediction = Forwardpass(x_test, Wh1, bj, Wh2, bl, Wo, bk)
         acc = AccTest(y_test, prediction)
         acc_history.append(acc)
         
         if ep % 10 == 0:
-            print(f"Epoch {ep}/{epochs} - Loss: {loss_history[ep-1]:.4f} - Accuracy: {acc_history[ep-1]:.4f}")
+            print(f"Epoch {ep}/{epochs} - Training Loss: {loss_history[ep-1]:.4f} - Test Accuracy: {acc_history[ep-1]:.4f}")
     
-    return Wh1, bh1, Wh2, bh2, Wo, bo, loss_history, acc_history
+    return Wh1, bj, Wh2, bl, Wo, bk, loss_history, acc_history
 
 # Train
-Wh1, bh1, Wh2, bh2, Wo, bo, loss_history, acc_history = train(
+Wh1, bj, Wh2, bl, Wo, bk, loss_history, acc_history = train(
     x_train, y_train, x_test, y_test, train_samples, miniBatch_size,
-    Wh1_init, bh1_init, Wh2_init, bh2_init, Wo_init, bo_init, epochs, lr
-)
+    Wh1_init, bj_init, Wh2_init, bl_init, Wo_init, bk_init, epochs, lr)
 
 #Evaluate
-_, _, _, _, o = Forwardpass(x_test, Wh1, bh1, Wh2, bh2, Wo, bo)
+_, _, _, _, o = Forwardpass(x_test, Wh1, bj, Wh2, bl, Wo, bk)
 accuracy = AccTest(y_test, o)*100
-print(f"Accuracy: {accuracy:.4f}%")
+print(f"Accuracy: {accuracy:.3f}%")
 
 # Visualize with plots
 plt.figure(figsize=(10, 5))
